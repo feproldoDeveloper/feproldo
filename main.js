b7573bd7a0e70a31898f20dfb0f8402478eaedd4
feproldoDeveloper <130132677+feproldoDeveloper@users.noreply.github.com>
2024-01-26 19:34:34 +0300

    5

---
+++ main.js
@@ -0,0 +1,46 @@
+const parent = document.querySelector('#title');
+const title = parent.querySelector("#title-titl")
+const desc = parent.querySelector("#title-desc")
+const img = document.getElementById("title-img-img")
+var isAnim = false;
+parent.style.display = "none"
+function js(){
+    if(isAnim) return
+    isAnim = true;
+    img.src = "js.jpg"
+    title.innerText = "JavaScript"
+    desc.innerText = "Создаю ботов."
+    parent.style.display = "block"
+    parent.style.animation = "titleAnimOpen 1s forwards"
+    setTimeout(offAnim, 5000)
+}
+function csharp(){
+    if(isAnim) return
+    isAnim = true;
+    img.src = "csharp.png"
+    title.innerText = "C#"
+    desc.innerText = "Работаю с Unity."
+    parent.style.display = "block"
+    parent.style.animation = "titleAnimOpen 1s forwards"
+    setTimeout(offAnim, 5000)
+}
+function unity(){
+    console.log("1")
+    if(isAnim) return
+    isAnim = true;
+    img.src = "unity.png"
+    title.innerText = "Unity"
+    desc.innerText = "Создаю игры."
+    parent.style.display = "block"
+    parent.style.animation = "titleAnimOpen 1s forwards"
+    setTimeout(offAnim, 5000)
+}
+function offAnim(){
+    parent.style.animation = "titleAnimClose 1s forwards"
+    setTimeout(offAnim1, 1000)
+}
+function offAnim1(){
+    parent.style.display = "none"
+    parent.style.animation = ""
+    isAnim = false  
+}
